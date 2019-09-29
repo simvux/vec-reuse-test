231b3c4733521b93a68c9d66b9ae8d57cbf98cc3
Simon Larsson <simon.larsson-general@protonmail.com>
2019-09-30 00:20:36 +0200

    Added some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![feature(test)]
+#![feature(const_vec_new)]
 
 use std::hint::black_box;
 use std::time::SystemTime;
+
+static mut VEC: Vec<usize> = Vec::new();
+static mut VEC2: Vec<usize> = Vec::new();
 
 // RESULTS:
 // if vec is reused 60+ times then FIRST METHOD is the most efficent
@@ -13,11 +17,28 @@ fn main() {
     std::io::stdin().read_line(&mut inp).unwrap();
     let n = inp.trim().parse::<usize>().unwrap();
 
+    unsafe {
+        // FIRST METHOD
+        let t = SystemTime::now();
+        VEC2.reserve(10);
+        for _ in 0..3 {
+            VEC2.push(0);
+        }
+        for i1 in 0..n {
+            for i in 0..3 {
+                VEC2[i] = i1 + n;
+                black_box(&VEC2);
+            }
+        }
+        let took = t.elapsed().unwrap();
+        println!("first(global) took: {:?}", took);
+    }
+
     // FIRST METHOD
     let t = SystemTime::now();
     let mut vec: Vec<usize> = vec![0; 10];
     for i1 in 0..n {
-        for i in 0..10 {
+        for i in 0..3 {
             vec[i] = i1 + n;
             black_box(&vec);
         }
@@ -25,12 +46,27 @@ fn main() {
     let took = t.elapsed().unwrap();
     println!("first took: {:?}", took);
 
+    unsafe {
+        // SECOND METHOD
+        let t = SystemTime::now();
+        VEC.reserve(10);
+        for i in 0..n {
+            VEC.clear();
+            for _ in 0..3 {
+                VEC.push(i + n);
+                black_box(&VEC);
+            }
+        }
+        let took = t.elapsed().unwrap();
+        println!("second(global) took: {:?}", took);
+    }
+
     // SECOND METHOD
     let t = SystemTime::now();
-    let mut vec: Vec<usize> = Vec::with_capacity(10);
+    let mut vec = Vec::with_capacity(10);
     for i in 0..n {
         vec.clear();
-        for _ in 0..10 {
+        for _ in 0..3 {
             vec.push(i + n);
             black_box(&vec);
         }
@@ -42,11 +78,23 @@ fn main() {
     let t = SystemTime::now();
     for i in 0..n {
         let mut vec: Vec<usize> = Vec::with_capacity(10);
-        for _ in 0..10 {
+        for _ in 0..3 {
             vec.push(i + n);
             black_box(&vec);
         }
     }
     let took = t.elapsed().unwrap();
     println!("third took: {:?}", took);
+
+    // FOURTH METHOD
+    let t = SystemTime::now();
+    let mut vec: [usize; 10] = [0; 10];
+    for i1 in 0..n {
+        for i in 0..3 {
+            vec[i] = i1 + n;
+            black_box(&vec);
+        }
+    }
+    let took = t.elapsed().unwrap();
+    println!("fourth took: {:?}", took);
 }
